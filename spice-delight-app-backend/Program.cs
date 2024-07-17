364090540350877ccb099827c4f25660d2358cde
Holmberg18 <holmberg18@gmail.com>
2024-07-16 21:58:22 -0700

    [+/-] Run migrations scripts on startup

--- spice-delight-app-backend/Program.cs
+++ spice-delight-app-backend/Program.cs
@@ -28,6 +28,22 @@ else
 
 var app = builder.Build();
 
+// Apply pending migrations on startup
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<SpiceDbContext>();
+        context.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while migrating the database.");
+    }
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
