f6b55d2113eafc94499038a9e1bcaed11975ca3b
Holmberg18 <holmberg18@gmail.com>
2024-07-16 21:40:06 -0700

    [+] Add GetProducts and GetProduct in ProductController

--- spice-delight-app-backend/Controllers/ProductController.cs
+++ spice-delight-app-backend/Controllers/ProductController.cs
@@ -10,5 +10,31 @@ namespace spice_delight_app_backend.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
+        private readonly SpiceDbContext _context;
+
+        public ProductController(SpiceDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Product
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        [HttpGet("{idMeal}")]
+        public async Task<ActionResult<Product>> GetProduct(int idMeal)
+        {
+            var product = await _context.Products.FindAsync(idMeal);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return product;
+        }
     }
 }
